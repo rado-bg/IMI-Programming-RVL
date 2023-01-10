97d9bc52a3c156a9cb35a673bc468b4f74a2d9ab
rado-bg <118369684+rado-bg@users.noreply.github.com>
2023-01-10 10:54:44 +0200

    Delete bitwise.h

--- Day5/task2/bitwise.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdint.h>
-#include <stdio.h>
-
-int checkBit(uint64_t mask, int bit);
-void setBit(uint64_t* mask, int bit);
-void clearBit(uint64_t* mask, int bit);
-void toggleBit(uint64_t* mask, int bit);
-void printMask(uint64_t mask);
